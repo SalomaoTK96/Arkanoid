ac5d0760558e2ae7edf4475356050908c046e037
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-17 20:42:39 -0300

    Create Bloco.h

---
+++ Cabeçalho/Bloco.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Estruturas.h"
+
+#ifndef BLOCO
+#define BLOCO
+
+void iniciarBloco(Bloco *bloco, float posx, float posy);
+
+void desenharBloco(Bloco *bloco);
+
+#endif
