c8172617ab24429ee2070cd66728e00abfa2be58
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-17 21:44:54 -0300

    Update Ranking.h

--- Cabeçalho/Ranking.h
+++ Cabeçalho/Ranking.h
@@ -4,6 +4,14 @@
 
 #include "Estruturas.h"
 
-struct atualizarRanking(Jogador *jogador)
+void iniciarRanking(Ranking *ranking, char *arquivo);
+
+void atualizarRanking(Ranking *ranking, Jogador *jogador);
+
+void desenharRanking(Ranking *ranking);
+
+void salvarRanking(Ranking *ranking);
+
+void carregarRanking(Ranking *ranking);
 
 #endif
