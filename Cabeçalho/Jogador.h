81afbf43b08808ae0ce83bda9e7ebce0dd58d94f
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-17 21:14:04 -0300

    Create Jogador.h

---
+++ Cabeçalho/Jogador.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "Estruturas.h"
+
+#ifndef JOGADOR
+#define JOGADOR
+
+void atualizarPontuacao(Jogador *jogador, int blocosEliminados)
+
+void atualizarTempo(Jogador *jogador, int tempoDecorrido)
+
+void atualizarVidas(Jogador *jogador, int vidasPerdidas)
+
+#endif
