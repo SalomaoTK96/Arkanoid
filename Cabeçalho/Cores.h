77ab4d0bacdd813043ee50138871ce60b0d0f97a
Arthur Soares Silva <arthurssilva1404@gmail.com>
2025-11-24 21:29:58 -0300

    Create Cores.h

---
+++ Cabeçalho/Cores.h
@@ -0,0 +1,32 @@
+#include "raylib.h"
+
+#ifndef CORES_H
+#define CORES_H
+
+Color obterCorBlocoForte();
+
+Color obterCorBlocoMedio();
+
+Color obterCorBlocoFraco();
+
+Color obterCorBola();
+
+Color obterCorBarra();
+
+Color obterCorItemVida();
+
+Color obterCorItemAumentar();
+
+Color obterCorItemDiminuir();
+
+Color obterCorItemPontos();
+
+Color obterCorItemInversor();
+
+Color obterCorFundo();
+
+Color obterCorTexto();
+
+Color obterCorTitulo();
+
+#endif
